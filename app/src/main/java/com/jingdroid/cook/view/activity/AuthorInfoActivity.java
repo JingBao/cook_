121a0b60de7857d57a0d53a021d59f0861e7c9f5
JingBao <1129785551@qq.com>
2017-07-26 18:09:31 +0800

    编辑详情开始

---
+++ app/src/main/java/com/jingdroid/cook/view/activity/AuthorInfoActivity.java
@@ -0,0 +1,15 @@
+package com.jingdroid.cook.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jingdroid.cook.R;
+
+public class AuthorInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_author_info);
+    }
+}
